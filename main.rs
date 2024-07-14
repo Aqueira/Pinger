9d0106a9343deb10c9ed9732782e56df491f93e1
Aqueira <162311168+Aqueira@users.noreply.github.com>
2024-07-14 23:16:29 +0300

    Update main.rs

--- main.rs
+++ main.rs
@@ -7,37 +7,41 @@ use dns_lookup::{lookup_host};
 fn main() -> Result<(), anyhow::Error> {
     initial_logger();
 
-    println!("Впишите ссылку");
+    println!("Write IPv4");
     let website = {
         let mut buffer = String::new();
-        io::stdin().read_line(&mut buffer).map_err(|e|{
-            log::error!("Ошибка ввода пользователя - {}", e);
-            anyhow::anyhow!("Ошибка ввода пользователя - {}", e)
+        io::stdin().read_line(&mut buffer).map_err(|err|{
+            log::error!("Error to get user input - {}", err);
+            anyhow::anyhow!("Error to get user input - {}", err)
         })?;
         buffer.trim().to_string()
     };
+    
     loop {
         println!("-------------------------------------------");
-        let _ = ping_website(&website).inspect_err(|e|{
-            log::error!("Error to ping website -> {}", e)
+        let _ = ping_website(&website).inspect_err(|err|{
+            log::error!("Error to ping website -> {}", err)
         });
         sleep(Duration::from_secs(10));
     }
 }
 
-fn initial_logger() {
-    env::set_var("RUST_LOG", "info");
-    env_logger::init();
-}
 
 fn ping_website(website: &str) -> Result<(), anyhow::Error> {
-    println!("Пинговка: {}", website);
-    let ips = lookup_host(website).map_err(|e|{
-        log::error!("Ошибка подключения к серверу - {}", e);
-        anyhow::anyhow!("Ошибка подключения к серверу - {}", e)
+    println!("Pinging: {}", website);
+    let ips = lookup_host(website).map_err(|err|{
+        log::error!("Error to get connect with server! - {}", err);
+        anyhow::anyhow!("Error to get connect with server! - {}", err)
     })?;
+    
     for ip in ips {
         log::info!("{} желаемый айпи", ip);
     }
+    
     Ok(())
+}
+
+fn initial_logger() {
+    env::set_var("RUST_LOG", "info");
+    env_logger::init();
 }
